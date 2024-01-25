2f828aab375d796346ca35ea8d95e3e4651db93c
Ben Faulkner <ben.faulkner1988@gmail.com>
2024-01-25 10:45:47 +0000

    my changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -33,7 +33,7 @@ export const register = async (req, res) => {
 
   const user = await User.create({ ...req.body, verificationToken });
 
-  const origin = "http://localhost:5173";
+  const origin = "http://localhost:5100";
 
   await sendVerificationEmail({
     name: user.name,
